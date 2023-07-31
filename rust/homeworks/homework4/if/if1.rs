7603be84df087d2a606a32cbc1b62d7a719fd53c
Bertrand <bertrandrigal.pro@gmail.com>
2023-07-31 19:49:30 +0200

    add rust exercices

--- rust/homeworks/homework4/if/if1.rs
+++ rust/homeworks/homework4/if/if1.rs
@@ -1,14 +1,35 @@
 // if1.rs
 
-// I AM NOT DONE
-
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
     // - additional variables
     // Execute `zustlings hint if1` for hints
+    // if (a > b) {
+    //     a
+    // } else {
+    //     b
+    // }
+    // use a match expression
 
+    // match a {
+    //     a if a > b => {
+    //         println!("a : {} b: {}",a, b);
+    //         return a;
+    //     },
+    //     a if b > a =>  {
+    //         println!("a : {} b: {}",a, b);
+    //         return b;
+    //     },
+    //     _ => panic!(),
+    // }
+
+    match a.cmp(&b) {
+        std::cmp::Ordering::Greater => a,
+        std::cmp::Ordering::Less => b,
+        std::cmp::Ordering::Equal => a,
+    }
 }
 
 // Don't mind this for now :)
